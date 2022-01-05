025a35e9e64122c4052ea171acd8daf302ffbae3
swifter <me@jingxuetao.com>
2022-01-05 09:18:24 +0800

    swift package

---
+++ Sources/ATCycleScrollView/ATCycleScrollView.swift
@@ -0,0 +1,6 @@
+public struct ATCycleScrollView {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/ATCycleScrollViewTests/ATCycleScrollViewTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ATCycleScrollView
+
+final class ATCycleScrollViewTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ATCycleScrollView().text, "Hello, World!")
+    }
+}
